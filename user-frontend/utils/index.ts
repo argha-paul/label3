76d591d0377f90c076143f53e40e1dce16d650c4
argha-paul <arghapaul77@gmail.com>
2024-09-25 21:36:32 +0530

    Update index.ts

--- user-frontend/utils/index.ts
+++ user-frontend/utils/index.ts
@@ -1,4 +1,4 @@
 
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "label3-backend.vercel.app";
 export const CLOUDFRONT_URL = "https://d13zjpigcskbyr.cloudfront.net";
 export const RPC_URL="https://solana-devnet.g.alchemy.com/v2/_D60iTxHlFPFtsDfw5l66yaD6Fb8LCjc"
